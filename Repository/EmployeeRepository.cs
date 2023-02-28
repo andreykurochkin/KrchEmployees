d140fccca152420f0443773de6c3d4e9fd3bc382
andreykurochkin <andrey.kurochkin@hotmail.com>
2023-02-28 21:09:48 +0300

    Handles 404 exception

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -13,4 +13,8 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
         FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .OrderBy(x => x.Name);
+
+    public Employee? GetEmployee(Guid companyId, Guid Id, bool trackChanges) =>
+        FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(companyId), trackChanges)
+            .SingleOrDefault();
 }
