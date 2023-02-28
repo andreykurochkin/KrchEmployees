0d9ac970af7983ca5c4d1a95838c42315a7d39be
andreykurochkin <andrey.kurochkin@hotmail.com>
2023-02-28 16:54:06 +0300

    Mutate http context

---
+++ Entities/Exceptions/CompanyNotFoundException.cs
@@ -0,0 +1,8 @@
+namespace Entities.Exceptions;
+
+public sealed class CompanyNotFoundException : NotFoundException
+{
+    public CompanyNotFoundException(Guid companyId) : base($"The company with id: {companyId} does not exist in persistent")
+    {
+    }
+}
